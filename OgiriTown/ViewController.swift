4564281b255e4d4ed333d4f7dbfbd3a4f187380e
naohisa0616 <naohisa0616@icloud.com>
2023-08-27 16:12:00 +0900

    Initial Commit

---
+++ OgiriTown/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OgiriTown
+//
+//  Created by 宮崎直久 on 2023/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
